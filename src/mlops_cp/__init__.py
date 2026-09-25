159fe659923521836ff4e43d7e51c98ac560d902
Maharshi Patel <pmaharshi999@gmail.com>
2026-09-25 02:03:10 +0200

    feat: expose world-class core capabilities

--- src/mlops_cp/__init__.py
+++ src/mlops_cp/__init__.py
@@ -1,3 +1,9 @@
+from .lifecycle import ALLOWED_TRANSITIONS, InvalidLifecycleTransition, validate_transition
 from .registry import ModelRegistry
 
-__all__ = ["ModelRegistry"]
+__all__ = [
+    "ALLOWED_TRANSITIONS",
+    "InvalidLifecycleTransition",
+    "ModelRegistry",
+    "validate_transition",
+]
