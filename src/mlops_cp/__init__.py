485d411d24e4f18b9f8e72bc5cf12763e5f378be
Maharshi Patel <pmaharshi999@gmail.com>
2026-09-25 02:05:53 +0200

    feat(mlops): expose deployment target interface

--- src/mlops_cp/__init__.py
+++ src/mlops_cp/__init__.py
@@ -1,8 +1,12 @@
+from .deployments import DeploymentCommand, DeploymentReceipt, HTTPDeploymentTarget
 from .lifecycle import ALLOWED_TRANSITIONS, InvalidLifecycleTransition, validate_transition
 from .registry import ModelRegistry
 
 __all__ = [
     "ALLOWED_TRANSITIONS",
+    "DeploymentCommand",
+    "DeploymentReceipt",
+    "HTTPDeploymentTarget",
     "InvalidLifecycleTransition",
     "ModelRegistry",
     "validate_transition",
